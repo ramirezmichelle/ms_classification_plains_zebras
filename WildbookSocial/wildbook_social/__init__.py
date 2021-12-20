97f387ad03179d3ce52babb50c9721cd00f81215
Michelle Ramirez <51290254+ramirezmichelle@users.noreply.github.com>
2021-12-20 12:16:43 -0600

    Update __init__.py

--- WildbookSocial/wildbook_social/__init__.py
+++ WildbookSocial/wildbook_social/__init__.py
@@ -1,20 +1,21 @@
 ## playgrounds + social media api's
-from .Youtube.youtube import YouTube
-from .iNaturalist.inaturalist import iNaturalist
+# from .Youtube.youtube import YouTube
+# from .iNaturalist.inaturalist import iNaturalist
 from .Flickr.flickr import Flickr
 
 
 ## database class & visuals (geospatial and temporal)
 # from .Database.database import Database
 from .Database.database import Database
-from .Database.visuals import Visualize
-from .Database.geospatial import Geospatial
+# from .Database.visuals import Visualize
+# from .Database.geospatial import Geospatial
 
 ## species classification
 from .SpeciesClassifier.species_classifier import SpeciesClassifier
 from .SpeciesClassifier.image_data import Image
 
-assert all((Flickr, iNaturalist, YouTube, Database, SpeciesClassifier, Image))
-#assert all((Flickr, iNaturalist, YouTube, Twitter, Database_Beta))
+# assert all((Flickr, iNaturalist, YouTube, Database, SpeciesClassifier, Image))
+# assert all((Flickr, iNaturalist, YouTube, Twitter, Database_Beta))
+assert all((Flickr, Database, SpeciesClassifier, Image))
 
 name = "wildbook_social"
